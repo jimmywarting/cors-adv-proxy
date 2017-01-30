cbb86868189e5ab372b3508018867d9a3c6b2b58
Jimmy Wärting <jimmy@warting.se>
2017-01-30 07:38:07 +0100

    wip

--- README.md
+++ README.md
@@ -1,22 +1,13 @@
-crossorigin.me
+cors-adv-proxy
 ==============
-[![Build
-Status](https://travis-ci.org/technoboy10/crossorigin.me.svg?branch=v2)](https://travis-ci.org/technoboy10/crossorigin.me)
 
-
-[![Join the chat at https://gitter.im/technoboy10/crossorigin.me](https://badges.gitter.im/technoboy10/crossorigin.me.svg)](https://gitter.im/technoboy10/crossorigin.me?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
-[![Follow on twitter](https://img.shields.io/badge/twitter-%40corsproxy-blue.svg)](https://twitter.com/corsproxy)
-[![Support me on patreon!](https://img.shields.io/badge/support-patreon-green.svg)](https://patreon.com/corsproxy)
-[![Donate bitcoin to support crossorigin.me](https://img.shields.io/badge/donate-bitcoin-yellow.svg)](https://coinbase.com/technoboy10)
-
-A CORS proxy replacement. Based partly on https://github.com/bmpvieira/simple-corsproxy.
+A CORS proxy replacement. Based partly on http://crossorigin.me.
 
 ## What?
-crossorigin.me is a CORS proxy. It lets developers (like you!) access resources from other sites that don't have CORS enabled on their server.
+Unlike other cors proxies this allows you to
+ 1. Use any http method you request
+ 2. Control exactly what headers are being sent n' received
+ 3. And last but not least: Override http method
 
-## Why?
-One of the more well known CORS proxies, corsproxy.com, recently disappeared. Quite a few things relied on that site, so I decided to set a new proxy up. 
-
-## Where?
-
-[Here](http://crossorigin.me).
+## How?
+ Good question
--- modules/proxy.js
+++ modules/proxy.js
@@ -1,16 +1,21 @@
 const request = require('request')
 const URLSearchParams = require('url-search-params')
 const Headers = require('fetch-headers')
+const urlParser = require('url').parse
 
 /*
   get handler handles standard GET reqs as well as streams
 */
 function get (req, res, next) {
+  if(req.method.toLowerCase() === 'options') return opts(req, res);
+
   let {url} = req
   let search = url.substr(url.indexOf('?') + 1)
   let params = new URLSearchParams(search)
+  let resHeaders = new Headers
+  let reqHeaders = new Headers
+  let method = params.get('_method') || req.method
 
-  res.header('Access-Control-Allow-Origin', '*'); // Actually do the CORS thing! :)
 
   url = params.get('url')
   if (!url) {
@@ -18,25 +23,65 @@ function get (req, res, next) {
     return res.end('url search param is required');
   }
 
-  // forward client headers to server
-  let headers = new Headers()
-  for (let header in req.headers) {
-    headers.append(header, req.headers[header])
+  if (!params.has('ignoreReqHeaders')) {
+    for (let header in req.headers) {
+      if(header.toLowerCase() !== 'host') {
+        reqHeaders.append(header, req.headers[header])
+      }
+    }
   }
 
-  let forwardedFor = headers.get('X-Fowarded-For')
-  headers.set('X-Fowarded-For', (forwardedFor ? forwardedFor + ',' : '') + req.connection.remoteAddress)
+  let forwardedFor = reqHeaders.get('X-Fowarded-For')
+  reqHeaders.set('X-Fowarded-For', (forwardedFor ? forwardedFor + ',' : '') + req.connection.remoteAddress)
 
-  request
-    .get(url, {headers}) // GET the document that the user specified
+  if (params.has('reqHeadersSet')) {
+    try {
+      let h = new Headers(JSON.parse(params.get('reqHeadersSet')))
+      for (let [key, val] of h) {
+        reqHeaders.set(key, val)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  if (params.has('reqHeaderDelete')) {
+    try {
+      let keys = JSON.parse(params.get('reqHeaderDelete'))
+      for (let key of keys) {
+        reqHeaders.delete(key)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+
+  let headers = Object.create(null)
+
+  for (let [k,v] of reqHeaders) {
+    headers[k] = v
+  }
+
+  request({url, headers, method, followRedirect: false}) // GET the document that the user specified
     .on('response', page => {
-      res.statusCode = page.statusCode;
+      res.statusCode = 200 // page.statusCode;
 
       // include only desired headers
+      let responseHeaders = new Headers
       for (let header in page.headers) {
-        res.header(header, page.headers[header])
-      }
+        if (header === 'location') key = 'location'
+        else if (header === 'set-cookie') key = 'set-cookie'
+        else key = header
 
+        responseHeaders.append(key, page.headers[header])
+      }
+      responseHeaders.set('Access-Control-Allow-Origin', '*');
+
+      for (let [key, val] of responseHeaders) {
+        res.header(key, val)
+      }
+      res.header('Access-Control-Expose-Headers', [...responseHeaders.keys()].join(','))
       // must flush here -- otherwise pipe() will include the headers anyway!
       res.flushHeaders()
     })
@@ -44,28 +89,47 @@ function get (req, res, next) {
 }
 
 /*
-post and put handlers both handle sending data to servers
-*/
-function post (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // Actually do the CORS thing! :)
-    next();
-}
-
-function put (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // Actually do the CORS thing! :)
-    next();
-}
-
-/*
 opts handler allows us to use our own CORS preflight settings
 */
-function opts (req, res, next) { // Couple of lines taken from http://stackoverflow.com/questions/14338683
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', req.header('Access-Control-Request-Method'));
-    res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'));
-    res.header('Access-Control-Max-Age', '86400'); // Cache preflight for 24 hrs if supported
-    res.send(200);
-    next();
+function opts (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', req.header('Access-Control-Request-Method'))
+    res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'))
+    res.header('Access-Control-Max-Age', '86400') // Cache preflight for 24 hrs if supported
+    res.send(200)
 }
 
-module.exports = {get, post, put, opts};
+module.exports = {get};
+
+/*
+var params = new URLSearchParams()
+params.append('url', 'http://httpbin.org/cookies/set?k2=v2&k1=v1')
+params.append('ignoreReqHeaders', '1')
+// params.append('_method', 'get')
+
+fetch('http://localhost:8080/?' + params, {})
+.then(res => console.log(res))
+*/
+
+
+
+
+var e = request({
+  // will be ignored
+  method: 'GET',
+  uri: 'http://httpbin.org/cookies/set?k2=v2&k1=v1',
+
+  // HTTP Archive Request Object
+  har: {
+    url: 'http://httpbin.org/cookies/set?k2=v2&k1=v1',
+    method: 'GET',
+    headers: [
+      {
+        name: 'content-type',
+        value: 'application/x-www-form-urlencoded'
+      }
+    ]
+  }
+}, function(a, e){
+  console.log(e);
+})
