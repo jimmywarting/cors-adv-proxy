d3119ce759758e079e81e05c4584c84a257e2cea
Jimmy Wärting <jimmy@warting.se>
2017-02-16 17:58:41 +0100

    Done!

--- README.md
+++ README.md
@@ -4,10 +4,64 @@ cors-adv-proxy
 A CORS proxy replacement. Based partly on http://crossorigin.me.
 
 ## What?
-Unlike other cors proxies this allows you to
- 1. Use any http method you request
+Making exactly the kind of request you want from a webpage can be tough...
+
+That is why we unlike other cors proxies allows you to
+ 1. Use any http method you want
  2. Control exactly what headers are being sent n' received
- 3. And last but not least: Override http method
 
 ## How?
- Good question
+With url params!
+
+```js
+var params = new URLSearchParams()
+params.set('url', url)
+
+// Useful if you want to extract the cookies that are being set durning a redirect
+// (just the present of `noFollow` param will prevent redirects)
+// http://httpbin.org/cookies/set?k2=v2&k1=v1
+params.set('noFollow', 'true')
+
+// If you are hot linking to something in a DOM tree you can only use GET method
+// using method will Override the request method
+// <iframe src="?method=post&url=http://httpbin.org/post"></iframe>
+params.set('method', 'GET')
+
+// By default request headers are being forwarded. If you don't want to send
+// them along b/c you want to be anonymous (due to referrer automatically) being
+// set unless configure the Referrer Policy. Origin also leaks information about
+// where the request where made from when doing ajax calls
+// (just the present of `ignoreReqHeaders` param will remove all headers)
+params.set('ignoreReqHeaders', 'true')
+
+// Another way of removing unwanted headers is by deleting them manually
+params.set('deleteReqHeader', '["Referrer", "Origin"]')
+
+// If you would like to override some unsafe headers that can't be set With
+// xhr.setRequestHeaders (https://www.w3.org/TR/2014/WD-XMLHttpRequest-20140130/#dom-xmlhttprequest-setrequestheader)
+// then you can use `setReqHeaders` param
+// The value accept the same argument `Headers` constructor allows
+// (2D array or key/val object)
+//
+// Here is the complete list of unsafe headers Ajax are not allowed to send:
+// Accept-Charset, Accept-Encoding, Access-Control-Request-Headers,
+// Access-Control-Request-Method, Connection, Content-Length, Cookie, Cookie2,
+// Date, DNT, Expect, Host, Keep-Alive, Origin, Referer, TE, Trailer,
+// Transfer-Encoding, Upgrade, User-Agent, Via
+var h = new Headers([['Cookie', 'x=1']])
+params.set('setReqHeaders', JSON.stringify([...h]))
+
+// Using setReqHeaders will override headers much like `new Headers().set`
+// If you just wish to add/append use addReqHeader
+// (works the same way as `new Headers().add`)
+
+// One alternative method to set unsafe headers without the params, Is prefixing
+// all unsafe headers with `X-Cors-` all prefix will be removed before sending
+fetch(url, {headers: {'X-Cors-Cookie': 'x=1'}})
+
+// If you are not getting dose headers you wish for then you can add, delete, or
+// set a value much like setReqHeaders, deleteReqHeader, and addReqHeader, only
+// difference is you change the Req to Res
+// Then you can get away with "refuse to execute script due to text/plain"
+// or get away with 
+```
--- modules/proxy.js
+++ modules/proxy.js
@@ -2,9 +2,31 @@ const request = require('request')
 const URLSearchParams = require('url-search-params')
 const Headers = require('fetch-headers')
 const urlParser = require('url').parse
+const Readable = require('stream').Readable
 
 /*
   get handler handles standard GET reqs as well as streams
+  order that it execute:
+
+  1 Request
+   1.0 Append all headers from the request (by stripping X-Cors-Req-)
+       unless ignoreHeaders
+   1.1 Set X-Forward-For ip adress
+   1.2 Delete unwanted request headers
+   1.3 add request headers from appendReqHeaders param
+   1.4 set request headers from setReqHeaders param
+   1.5 Override method if the `method` param exist
+   1.6 Override body if the `body` param exist
+   1.7 disable follow redirect if the `noFollow` param exist
+   1.8 execute the request and pipe body to the request
+ 2 Response
+  2.0 Append all headers from the response
+      (by replaceing some unreadable headers with X-Cors-Res)
+  2.1 Delete unwanted response headers
+  2.2 add response headers from appendResHeaders param
+  2.3 set response headers from setResHeaders param
+  2.5 set Expose-Headers to all headers that should be exposed
+  2.6 set Allow-Origin to *
 */
 function get (req, res, next) {
   if(req.method.toLowerCase() === 'options') return opts(req, res);
@@ -14,78 +36,173 @@ function get (req, res, next) {
   let params = new URLSearchParams(search)
   let resHeaders = new Headers
   let reqHeaders = new Headers
-  let method = params.get('_method') || req.method
+  let method = params.get('method') || req.method // 1.5
+  let body
+  let followRedirect = !params.get('noFollow') // 1.7
+  let deleteResHeader = []
+  let appendResHeader = []
+  let setResHeader = []
 
 
-  url = params.get('url')
-  if (!url) {
-    res.statusCode = 403;
-    return res.end('url search param is required');
+
+  // Validate deleteResHeader
+  if (params.has('deleteResHeader')) {
+    try {
+      let deleteResHeader = JSON.parse(params.get('deleteReqHeader'))
+    } catch (e) {
+      res.statusCode = 403
+      res.header('Access-Control-Allow-Origin', '*')
+      return res.end('Expected deleteResHeader to be an array with strings')
+    }
   }
 
+  // Validate deleteResHeader
+  if (params.has('appendResHeader')) {
+    try {
+      appendResHeader = new Headers(JSON.parse(params.get('appendResHeaders')))
+    } catch (e) {
+      res.statusCode = 403
+      res.header('Access-Control-Allow-Origin', '*')
+      return res.end('Failed to construct `new Headers` with appendResHeaders value')
+    }
+  }
+
+  // Validate setResHeader
+  if (params.has('setResHeader')) {
+    try {
+      setResHeader = new Headers(JSON.parse(params.get('setResHeaders')))
+    } catch (e) {
+      res.statusCode = 403
+      res.header('Access-Control-Allow-Origin', '*')
+      return res.end('Failed to construct `new Headers` with setResHeaders value')
+    }
+  }
+
+  url = params.get('url')
+
+  if (!url) {
+    res.statusCode = 403
+    res.header('Access-Control-Allow-Origin', '*')
+    return res.end('url search param is required')
+  }
+
+  // 1.0
   if (!params.has('ignoreReqHeaders')) {
     for (let header in req.headers) {
       if(header.toLowerCase() !== 'host') {
-        reqHeaders.append(header, req.headers[header])
+        reqHeaders.append(header.replace(/^x-cors-req-/i,''), req.headers[header])
       }
     }
   }
 
+  // 1.1
   let forwardedFor = reqHeaders.get('X-Fowarded-For')
   reqHeaders.set('X-Fowarded-For', (forwardedFor ? forwardedFor + ',' : '') + req.connection.remoteAddress)
 
-  if (params.has('reqHeadersSet')) {
+  // 1.2
+  if (params.has('deleteReqHeaders')) {
     try {
-      let h = new Headers(JSON.parse(params.get('reqHeadersSet')))
-      for (let [key, val] of h) {
-        reqHeaders.set(key, val)
-      }
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  if (params.has('reqHeaderDelete')) {
-    try {
-      let keys = JSON.parse(params.get('reqHeaderDelete'))
+      let keys = JSON.parse(params.get('deleteReqHeaders'))
       for (let key of keys) {
         reqHeaders.delete(key)
       }
     } catch (e) {
-      console.log(e)
+      res.statusCode = 403
+      res.header('Access-Control-Allow-Origin', '*')
+      return res.end('Expected deleteReqHeader to be an array with strings')
     }
   }
 
+  // 1.3
+  if (params.has('appendReqHeaders')) {
+    try {
+      let h = new Headers(JSON.parse(params.get('appendReqHeaders')))
+      for (let [key, val] of h) {
+        reqHeaders.append(key, val)
+      }
+    } catch (e) {
+      res.statusCode = 403
+      res.header('Access-Control-Allow-Origin', '*')
+      return res.end('Failed to construct `new Headers` with appendReqHeaders value')
+    }
+  }
 
+  // 1.4
+  if (params.has('setReqHeaders')) {
+    try {
+      let h = new Headers(JSON.parse(params.get('setReqHeaders')))
+      for (let [key, val] of h) {
+        reqHeaders.set(key, val)
+      }
+    } catch (e) {
+      res.statusCode = 403
+      res.header('Access-Control-Allow-Origin', '*')
+      return res.end('Failed to construct `new Headers` with setReqHeaders value')
+    }
+  }
+
+  // 1.6
+  if (params.has('body')) {
+    body = new Readable()
+    body.push(params.has('body'))
+    body.push(null)
+  } else {
+    body = req
+  }
+
+  // convert Headers to a object to make request able to handle them
   let headers = Object.create(null)
-
   for (let [k,v] of reqHeaders) {
     headers[k] = v
   }
 
-  request({url, headers, method, followRedirect: false}) // GET the document that the user specified
+  // 1.8
+  var opts = {url, headers, method, followRedirect}
+  body.pipe(request(opts) // GET the document that the user specified
     .on('response', page => {
-      res.statusCode = 200 // page.statusCode;
+      res.statusCode = page.statusCode;
 
-      // include only desired headers
+      // 2.0
       let responseHeaders = new Headers
       for (let header in page.headers) {
         if (header === 'location') key = 'location'
-        else if (header === 'set-cookie') key = 'set-cookie'
+        else if (header.toLowerCase() === 'set-cookie') key = 'X-Cors-Res-' + header
         else key = header
 
         responseHeaders.append(key, page.headers[header])
       }
-      responseHeaders.set('Access-Control-Allow-Origin', '*');
+
+      // TODO: maybe ignore response header
+
+      // 2.1
+      for (let key of deleteResHeader) {
+        responseHeaders.delete(key)
+      }
+
+      // 2.2
+      for (let [key, val] of appendResHeader) {
+        responseHeaders.append(key, val)
+      }
+
+      // 2.3
+      for (let [key, val] of setResHeader) {
+        responseHeaders.set(key, val)
+      }
+
+
+      /*
+       * Override everything else and expose all headers
+       */
+      responseHeaders.set('Access-Control-Expose-Headers', [...responseHeaders.keys()].join(','))
+      responseHeaders.set('Access-Control-Allow-Origin', '*')
 
       for (let [key, val] of responseHeaders) {
         res.header(key, val)
       }
-      res.header('Access-Control-Expose-Headers', [...responseHeaders.keys()].join(','))
+
       // must flush here -- otherwise pipe() will include the headers anyway!
       res.flushHeaders()
-    })
-    .pipe(res) // Stream requested url to response
+    })).pipe(res)
 }
 
 /*
@@ -99,37 +216,4 @@ function opts (req, res, next) {
     res.send(200)
 }
 
-module.exports = {get};
-
-/*
-var params = new URLSearchParams()
-params.append('url', 'http://httpbin.org/cookies/set?k2=v2&k1=v1')
-params.append('ignoreReqHeaders', '1')
-// params.append('_method', 'get')
-
-fetch('http://localhost:8080/?' + params, {})
-.then(res => console.log(res))
-*/
-
-
-
-
-var e = request({
-  // will be ignored
-  method: 'GET',
-  uri: 'http://httpbin.org/cookies/set?k2=v2&k1=v1',
-
-  // HTTP Archive Request Object
-  har: {
-    url: 'http://httpbin.org/cookies/set?k2=v2&k1=v1',
-    method: 'GET',
-    headers: [
-      {
-        name: 'content-type',
-        value: 'application/x-www-form-urlencoded'
-      }
-    ]
-  }
-}, function(a, e){
-  console.log(e);
-})
+module.exports = {get}
